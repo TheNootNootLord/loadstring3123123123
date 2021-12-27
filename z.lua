d241b77143c448e30174bf69cc9884101d5291c4
TheNootNootLord <96509769+TheNootNootLord@users.noreply.github.com>
2021-12-27 22:38:03 +1100

    i am an idiot smh

--- z.lua
+++ z.lua
@@ -149,7 +149,7 @@ game.Players:Chat('h \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n
 wait(0.2)
 game.Players:Chat('h \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  desk o rd . gg/ PAT nx vQ 9 Se')
 wait(0.2)
-game.Players:Chat('h \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n CoolKid on bottom, NootNoot on second, NoobSploi_t on TOP, CRY ABOUT IT
+game.Players:Chat('h \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n CoolKid on bottom, NootNoot on second, NoobSploi_t on TOP, CRY ABOUT IT')
 game.Players:Chat(".shutdown")
 
 wait(6) -- this is to prevent server hopping instantly lol
